2fdb498d3acea714dcb3e263cb38f29824d35d51
coltonsmith135 <115854214+coltonsmith135@users.noreply.github.com>
2022-12-22 21:26:56 -0800

    added javascript and html code

--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,8 @@ var curbsidePickupEl = document.querySelector('#curbside');
 var ratingEl = document.querySelector('#rating');
 var priceEl = document.querySelector('#price');
 var takesResEl = document.querySelector('#reservation');
+var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=seattle&term=starbucks&price=1";
+var apiKey = "DK0XjQWdA4BqtE9d8Bp8a-RVwjtf7EeHoXtmo6S1cGS2b4Atc_qf6kTCwKoH0JFdOW-ocv4yoBAudpgHJ5R6VTjT7FXB14gcEsrsT3KfdwnoOh7RVn3olYulhCelY3Yx";
 
 
 //Map Options (Create Variable to be input to change lat and lng)
@@ -47,35 +49,88 @@ const detailWindow = new google.maps.InfoWindow
   content: '<h2> Space Needle <h2>'
 })
 
- // marker.addEventListener("mouseover", () =>
- // {
-  //  detailWindow.open(map,marker)
- // })
 
-  
+
+ if(navigator.geolocation) {
+  console.log('geolocation is here!');
+
+  navigator.geolocation.getCurrentPosition((loc) => {
+      location.lat = loc.coords.latitude;
+      location.lng = loc.coords.longitude;
+
+      map = new google.maps.Map(document.getElementById("map"), options); 
+  },
+  (err) => {
+      console.log('user clocked no lol');
+      map = new google.maps.Map(document.getElementById('map'), options);
+  }
+  )
+} else {
+  console.log('geolocation is not supported :(');
+  map = new google.maps.Map(document.getElementById('map'), options);
+}
+
+
+
+autocomplete = new google.maps.places.Autocomplete(document.getElementById('city'), {
+
+  componentRestriction: {'country': ['us']},
+  fields: ['geometry', 'name'],
+  types: ['establishment']
+});
+
+autocomplete.addListener('place_changed', () => {
+  const place = autocomplete.getPlace();
+  new google.maps.Marker({
+       position: place.geometry.location,
+       title: place.name,
+       map: map
+      
+  })
+});
 
 }
 
 searchBtn.addEventListener('click', function() {
     console.log(cityEl.value);
     getRestaurants();
+    initMap();
 })
 
-var apiKey = 'AIzaSyBIkD8TRPCnwLsy4XuUqCY5UBPpQKqFktY';
-
-function getRestaurants (city) {
-   var url = `https://maps.googleapis.com/maps/api/place/textsearch/json?fields=formatted_address%2Cname&curbside-pickup&price_level&rating&serves_beer&serves_breakfast&serves_dinner&serves_lunch&serves_vegetarian_food&takeout&delivery&input=Restaurant&inputtype=textquery&key=AIzaSyBIkD8TRPCnwLsy4XuUqCY5UBPpQKqFktY`
-   console.log(url);
-   fetch(url)
-      .then(function (response) {
-        console.log(response)
-         return response.json()
-      }).then(function (data) {
-         console.log(data);
 
 
-      })
-  };
 
-// possible link
-// https://maps.googleapis.com/maps/api/place/textsearch/json?fields=formatted_address%2Cname&curbside-pickup&price_level&rating&serves_beer&serves_breakfast&serves_dinner&serves_lunch&serves_vegetarian_food&takeout&delivery&input=Restaurant&inputtype=textquery&key=AIzaSyBIkD8TRPCnwLsy4XuUqCY5UBPpQKqFktY
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    headers: {
+        "accept": "application/json",
+        "x-requested-with": "xmlhttprequest",
+        "Access-Control-Allow-Origin":"*",
+        "Authorization": `Bearer ${apiKey}`
+     },
+   success: function(result){
+        console.log(result);
+    }
+ });
+  
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     accept: 'application/json',
+  //     Authorization: 'Bearer DK0XjQWdA4BqtE9d8Bp8a-RVwjtf7EeHoXtmo6S1cGS2b4Atc_qf6kTCwKoH0JFdOW-ocv4yoBAudpgHJ5R6VTjT7FXB14gcEsrsT3KfdwnoOh7RVn3olYulhCelY3Yx'
+  //   }
+  // };
+  
+  fetch('https://api.yelp.com/v3/businesses/search?location=seattle&term=starbucks&price=1', ajax)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+
+getRestaurants();
+// Client ID
+// QAAEdheiexNQPRBhWZ1d3Q
+
+// API Key
+// DK0XjQWdA4BqtE9d8Bp8a-RVwjtf7EeHoXtmo6S1cGS2b4Atc_qf6kTCwKoH0JFdOW-ocv4yoBAudpgHJ5R6VTjT7FXB14gcEsrsT3KfdwnoOh7RVn3olYulhCelY3Yx
